33f773905c3a2cc414c49e61c522a4cfae0ec51e
liaochunchen <leocc325@gmail.com>
2025-11-23 17:42:35 +0800

    新增一个ValuePack的=重载用于直接使用double赋值

--- UnitConvertor.hpp
+++ UnitConvertor.hpp
@@ -8,49 +8,49 @@ class ValuePack;
 struct UnitProperty;
 namespace UnitConvertor
 {
-    enum DecimalRatio{Nano,Micro,Milli,One,Kilo,Mega,Giga,RatioNum};
+enum DecimalRatio{Nano,Micro,Milli,One,Kilo,Mega,Giga,RatioNum};
 
-    static const std::string DecimalRatioString[RatioNum] = {"n" , "u" , "m" , "" , "k" , "M" , "G"};
+static const std::string DecimalRatioString[RatioNum] = {"n" , "u" , "m" , "" , "k" , "M" , "G"};
 
-    enum DecimalUnit{Null,Freq,Time,Ampl,Voltage,Current,Phase,SampRate,VolArea,UnitNum};
+enum DecimalUnit{Null,Freq,Time,Ampl,Voltage,Current,Phase,SampRate,VolArea,UnitNum};
 
-    static const std::string DecimalUnitString[UnitNum] = {"" , "Hz" , "s" , "Vpp" , "V" , "A" , "°","Sa/s","V*s"};
+static const std::string DecimalUnitString[UnitNum] = {"" , "Hz" , "s" , "Vpp" , "V" , "A" , "°","Sa/s","V*s"};
 
-    ///这个函数返回一个单位对应的属性:属性包括这个单位所对应的最大数量级、最小数量级、单位枚举
-    static const UnitProperty generateUnitProperty(DecimalUnit unit) noexcept;
+///这个函数返回一个单位对应的属性:属性包括这个单位所对应的最大数量级、最小数量级、单位枚举
+const UnitProperty generateUnitProperty(DecimalUnit unit) noexcept;
 
-    ///将一个字符串转换为数据包,如果在调用这个函数的时候指定单位类型执行效率将会更高
-    ValuePack fromString(const std::string& target,DecimalUnit unit = DecimalUnit::UnitNum);
+///将一个字符串转换为数据包,如果在调用这个函数的时候指定单位类型执行效率将会更高
+ValuePack fromString(const std::string& target,DecimalUnit unit = DecimalUnit::UnitNum);
 
-    ///将当前数据的数量级转换为newRatio表示的数据
-    ValuePack ratioTo(ValuePack pack, DecimalRatio newRatio);
+///将当前数据的数量级转换为newRatio表示的数据
+ValuePack ratioTo(ValuePack pack, DecimalRatio newRatio);
 
-    ///将当前数据字符串的数量级转换为newRatio表示的数据
-    ValuePack ratioTo(const std::string& str, DecimalRatio newRatio);
+///将当前数据字符串的数量级转换为newRatio表示的数据
+ValuePack ratioTo(const std::string& str, DecimalRatio newRatio);
 
-    ///将数值自动转换为一个恰当单位表示的数值(1～999之间的值)
-    ValuePack proper(ValuePack pack);
+///将数值自动转换为一个恰当单位表示的数值(1～999之间的值)
+ValuePack proper(ValuePack pack);
 
-    ///将字符串自动转换为一个恰当单位表示的数值(1～999之间的值)
-    ValuePack proper(const std::string& str);
+///将字符串自动转换为一个恰当单位表示的数值(1～999之间的值)
+ValuePack proper(const std::string& str);
 
-    ///将ValuePack转换为字符串,不控制格式
-    std::string toString(const ValuePack& pack);
+///将ValuePack转换为字符串,不控制格式
+std::string toString(const ValuePack& pack);
 
-    ///将ValuePack转换为字符串,当fixedDecimal为true时精确到小数点后precision位,否则位保留fixedDecimal位有效数字
-    std::string toFormatString(const ValuePack& pack,int precision,bool fixedDecimal = true);
+///将ValuePack转换为字符串,当fixedDecimal为true时精确到小数点后precision位,否则位保留fixedDecimal位有效数字
+std::string toFormatString(const ValuePack& pack,int precision,bool fixedDecimal = true);
 
-    ///将ValuePack转换为字符串同时确保整数部分和小数部分长度固定,长度不足时填充给定字符
-    std::string toFormatString(const ValuePack& pack,int totalLeng,int decimalLen,char fill = '0');
+///将ValuePack转换为字符串同时确保整数部分和小数部分长度固定,长度不足时填充给定字符
+std::string toFormatString(const ValuePack& pack,int totalLeng,int decimalLen,char fill = '0');
 
-    ///将ValuePack转换为科学计数法的字符串,不控制格式
-    std::string toScientificString(const ValuePack& pack);
+///将ValuePack转换为科学计数法的字符串,不控制格式
+std::string toScientificString(const ValuePack& pack);
 
-    ///将ValuePack转换为科学计数法的字符串,小数点后面保留precision位
-    std::string toScientificString(const ValuePack& pack,int precision);
+///将ValuePack转换为科学计数法的字符串,小数点后面保留precision位
+std::string toScientificString(const ValuePack& pack,int precision);
 
-    ///将ValuePack转换为整数
-    long long toInt(const ValuePack& pack);
+///将ValuePack转换为整数
+long long toInt(const ValuePack& pack);
 };
 namespace Uc =  UnitConvertor;
 
@@ -75,17 +75,6 @@ public:
         this->m_Value = value;
         this->m_Ratio = ratio;
         this->m_Property = UnitConvertor::generateUnitProperty(unit);
-    }
-
-    bool operator==(const ValuePack& other) const noexcept
-    {
-        return std::abs(this->m_Value - other.m_Value) < 1e-10
-            &&this->m_Ratio == other.m_Ratio
-            && this->m_Property.unit == other.m_Property.unit;
-    }
-
-    bool operator!=(const ValuePack& other) const noexcept{
-        return !(*this == other);
     }
 
     ///可以直接与数值类型变量相加
@@ -137,11 +126,30 @@ public:
         return tmpPack;
     }
 
+    ValuePack& operator = (double value) noexcept
+    {
+        m_Value = value;
+        return *this;
+    }
+
+    bool operator==(const ValuePack& other) const noexcept
+    {
+        return std::abs(this->m_Value - other.m_Value) < 1e-10
+            &&this->m_Ratio == other.m_Ratio
+            && this->m_Property.unit == other.m_Property.unit;
+    }
+
+    bool operator!=(const ValuePack& other) const noexcept{
+        return !(*this == other);
+    }
+
     operator double() const noexcept { return m_Value; }
 
     double value() const noexcept{return this->m_Value;}
 
     UnitConvertor::DecimalRatio ratio() const noexcept{return this->m_Ratio;}
+
+    UnitConvertor::DecimalUnit unit() const noexcept{return this->m_Property.unit;}
 
     UnitProperty property() const noexcept{return this->m_Property;}
 
